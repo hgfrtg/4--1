3ca1ba64d072f650026778573bd591f7a03aa9c6
hgfrtg <jrbevknf@outlook.com>
2019-11-11 14:11:32 +0900

    [Finish] 4-9

---
+++ 4-9/config/initializers/application_controller_renderer.rb
@@ -0,0 +1,10 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+
+Refile.secret_key = '0bcc2ad2000976d8f48293c0fac98b5e07270d2a7b5558c62610bf37fd1aa2573584d0ce4ab24736da75e6006c9b1ddb2ceeb2c1e04dc7c89756411cb86c6cad'
